f46c8626f3a3f7b449869e40cc09b4f17dc9661f
taruto <taruto1215@gmail.com>
2018-09-03 15:16:05 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  inclusde SessionsHelper
+  include SessionsHelper
   
   private
   
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
      redirect_to login_url
     end
   end  
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  root to: 'toppages#index'
+  
+  get 'microposts/create'
+  get 'microposts/destroy'
 
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessoins#create'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
+  resources :microposts, only: [:create, :destroy]
 end
